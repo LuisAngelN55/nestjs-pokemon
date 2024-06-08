0f58edcf1e78405d8ab5bf5d1c9770458a145d02
Luis Angel <lang@zuntrix.com>
2024-06-08 13:30:21 -0500

    sección 7 terminada

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,17 @@
-export class CreatePokemonDto {}
+
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+
+    //isInt, isPositive, min 1
+    @IsInt(({ message: 'Value must be an int'}))
+    @IsPositive(({ message: 'Must be Positive int'}))
+    @Min(1, ({ message: 'Must greather than 0'}))
+    no: number;
+
+
+    // isString, minleth 1
+    @IsString(({ message: 'The name must be a string'}))
+    @MinLength(1, ({message: 'Min length 1'}))
+    name: string;
+}
